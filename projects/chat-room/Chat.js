fb773e53eeeb447117cb12b625091730aaf8a762
Dmitry <boombapkilla@yandex.ru>
2021-08-01 13:21:31 +0300

    add UserName

---
+++ projects/chat-room/Chat.js
@@ -0,0 +1,22 @@
+
+export default class Chat {
+    constructor() {
+    this.ui = {
+        loginWindow: new LoginWindow(
+            document.querySelector('#login'), this.onLogin.bind(this)
+        ),
+        mainWindow: new MainWindow(
+            document.querySelector('#main'), this.onLogin.bind(this)
+        ),
+        userName: new UserName(
+            document.querySelector('[data-role=user-name]'), this.onLogin.bind(this)
+        )
+    }
+    this.ui.loginWindow.show()
+};
+    async onLogin(name) {
+        this.ui.loginWindow.hide()
+        this.ui.mainWindow.show()
+        this.ui.userName.set(name)
+    }
+}
